eba71b6a3e5f5d48df703555352d5fcdaa5201b2
Harkirat.4373 <Harkirat.4373@tws.local>
2024-09-25 15:01:43 +0530

    added auth test endpoint

--- app/routes.js
+++ app/routes.js
@@ -1,7 +1,16 @@
-module.exports = function(app) {
 
-    app.get('/',function(req,res){ // İSTEMCİNİN '/' İSTEĞİNE KARŞILIK VEREN KOD BLOĞU
-        res.render('index'); // INDEX VİEW DOSYASI RENDER EDİLDİ
+
+module.exports = function(app) {
+    app.get('/',function(req,res){ 
+        res.render('index'); 
     });
 
+    app.post('/' ,(req,res)=>{
+        console.log(req.headers.authorization)
+        if (req.headers.authorization == 'test'){
+            res.status(200).send('authorized')
+        }else{
+            res.status(401).send('unauthorized')
+        }
+    })
 }
